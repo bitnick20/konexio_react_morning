9e91fb71b2971a8733cd656e3a6ac20180f86bae
bitnick20 <65895288+bitnick20@users.noreply.github.com>
2020-07-20 21:34:15 +0200

    add react morning

--- src/App.js
+++ src/App.js
@@ -1,26 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
+import Counter from './components/Counter';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+
+  constructor (props) {
+    super (props);
+
+    this.incrementCount = this.incrementCount.bind(this);
+
+    this.state = {
+      count: 1,
+    };
+  }
+  
+  
+  incrementCount = () => {
+    console.log("Je fais un test pour afficher ce message");
+    this.setState({
+      count: this.state.count +1})
+  }
+
+  decrementCount = () => {
+    console.log("Je fais un autre test pour voir")
+    this.setState({
+      count: this.state.count -1})
+  }
+  
+
+  render () {
+    return (
+      <div>
+        <h1 >Counter</h1>
+        {/* <h2>{this.state.count}</h2> */}
+        {/* <button onClick={this.incrementCount}>+</button>
+        <button onClick={this.decrementCount}>-</button> */}
+        <Counter incrementFn={this.incrementCount} 
+        decrementFn={this.decrementCount} 
+        count={this.state.count}/>
+      </div>
+    );
+  }
 }
 
 export default App;
